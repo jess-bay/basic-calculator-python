4837e665d2a9acbc1c2ddcc61b3c97e0696ff788
Jess Bayman <justjessb@gmail.com>
2024-07-16 13:41:05 +1200

    First step for the calculator: define functions

---
+++ python_app.py
@@ -0,0 +1,26 @@
+# functions needed - add, subtract, multiply, divide
+
+#simple: 
+# def add(a, b):
+#   return a + b
+
+def add(a, b): 
+  answer = a + b
+  print(str(a) + " + " + str(b) + " = " + str(answer))
+
+def subtract(a, b): 
+  answer = a - b
+  print(str(a) + " - " + str(b) + " = " + str(answer))
+
+def multiply(a, b): 
+  answer = a * b
+  print(str(a) + " x " + str(b) + " = " + str(answer))
+
+def divide(a, b): 
+  answer = a / b
+  print(str(a) + " / " + str(b) + " = " + str(answer))
+
+# print the options - add, subtract, multiply, divide, exit
+# get values
+# call functions with values retrieved
+# while loop to continue the program until exit
