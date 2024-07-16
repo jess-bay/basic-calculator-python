5239c27c61c1b13c0e597fa2cb4a3fc40d592e77
Jess Bayman <justjessb@gmail.com>
2024-07-16 13:45:03 +1200

    Add choices and conditional statements

--- python_app.py
+++ python_app.py
@@ -21,6 +21,38 @@ def divide(a, b):
   print(str(a) + " / " + str(b) + " = " + str(answer))
 
 # print the options - add, subtract, multiply, divide, exit
+  print("A. Addition")
+print("B. Subtraction")
+print("C. Multiplication")
+print("D. Division")
+print("E. Exit")
+
+choice = input("input your choice: ")
+
+if choice == "a" or choice == "A":
+  print("Addition")
+  a = int(input("input first number: "))
+  b = int(input("input second number: "))
+  add(a, b)
+elif choice == "b" or choice =="B":
+  print("Subtraction")
+  a = int(input("input first number: "))
+  b = int(input("input second number: "))
+  subtract(a, b)
+elif choice == "c" or choice == "C":
+  print("Multiplication")
+  a = int(input("input first number: "))
+  b = int(input("input second number: "))
+  multiply(a, b)
+elif choice == "d" or choice == "D":
+  print("Division")
+  a = int(input("input first number: "))
+  b = int(input("input second number: "))
+  divide(a, b)
+elif choice == "e" or choice == "E":
+  print("Exit")
+  quit()
+
 # get values
 # call functions with values retrieved
 # while loop to continue the program until exit
